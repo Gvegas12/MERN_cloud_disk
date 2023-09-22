dc7580334b258da357c20ab81db9144057918ddc
abdukarimov.h2 <abdukarimov.h2@wildberries.work>
2023-09-22 19:26:46 +0300

    feat: [server] add AuthRouter

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,26 +5,43 @@ const { check, validationResult } = require("express-validator");
 
 const router = new Router();
 
-router.post("/registration", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  "/registration",
+  [
+    check("email", "Некорректный email").isEmail(),
+    check(
+      "password",
+      "Пароль должен содержать не менее 3 символов и менее 12 символов"
+    ).isLength({ min: 3, max: 12 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = User.findOne({ email });
-    if (candidate) {
-      return res
-        .status(400)
-        .json({ message: "Пользователь с таким email уже существует" });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Некорректный запрос", errors });
+      }
+
+      const { email, password } = req.body;
+
+      const candidate = User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Пользователь с таким email уже существует" });
+      }
+
+      const hashPassword = await bcrypt.hash(password, 15);
+      const user = new User({ email, password: hashPassword });
+      т;
+
+      await user.save();
+      return res.json({ message: "Пользователь успешно создан" });
+    } catch (e) {
+      console.log(e);
+      res.send({ message: "Server error" });
     }
-
-    const hashPassword = await bcrypt.hash(password, 15);
-    const user = new User({ email, password: hashPassword });
-    т;
-
-    await user.save();
-    return res.json({ message: "Пользователь успешно создан" });
-  } catch (e) {
-    console.log(e);
-    res.send({ message: "Server error" });
-    ``;
   }
-});
+);
+
+module.exports = router;
